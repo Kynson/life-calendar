412c9f01853b54e133d4d46bc94b67049708ba86
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2024-06-27 15:52:36 +0800

    [Skip CI] Setup project
    - Further setup project so that it can work on both browser and Cloudflare workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import type { Plugin } from 'vite';
+
+import { svelte as sveltePlugin } from '@sveltejs/vite-plugin-svelte';
+import tsConfigPathPlugin from 'vite-tsconfig-paths';
+
+// This plugin bridge the difference between browser and Cloudflare Workers before ES module integration proposal for web assembly is supported by Vite
+function wasmPlugin(): Plugin {
+  return {
+    name: 'vite-plugin-wasm',
+    resolveId(source) {
+      if (!source.toLocaleLowerCase().endsWith('.wasm')) {
+        return null;
+      }
+
+      return this.resolve(source);
+    },
+    async load(id) {
+      if (!id.toLocaleLowerCase().endsWith('.wasm')) {
+        return null;
+      }
+
+      // URL will be a base64 url if the wasm size is smaller than assetsInlineLimit
+      return `import wasmUrl from '${id}?url';
+const wasmBuffer = await fetch(wasmUrl).then((response) => response.arrayBuffer());
+const wasmModule = await WebAssembly.compile(wasmBuffer);
+export default wasmModule;`;
+    }
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  build: {
+    target: 'esnext'
+  },
+  plugins: [
+    sveltePlugin(),
+    wasmPlugin(),
+    tsConfigPathPlugin()
+  ],
 })
